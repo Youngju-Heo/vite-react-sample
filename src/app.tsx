e333d83d34d1e1c473da320355296b89046fce8f
yjheo <yjheo@rainno.co.kr>
2023-04-03 17:37:55 +0900

    first commit

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
-import {Alert, Center, Text, VStack} from "@chakra-ui/react";
+import {
+  Alert, Button,
+  Center, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader,
+  DrawerOverlay, Flex,
+  IconButton, Input, Spacer,
+  Text,
+  useDisclosure,
+  VStack
+} from "@chakra-ui/react";
+import {MdLogout, MdMenu} from 'react-icons/md';
 
 import {Counter} from "./features/counter/counter";
 
@@ -8,25 +17,62 @@ import viteLogo from '/vite.svg'
 import reactLogo from './assets/react.svg'
 
 const App = (): JSX.Element => {
+  const {isOpen, onOpen, onClose} = useDisclosure();
+  console.log('menu status', isOpen);
+  const btnRef = React.useRef<HTMLButtonElement>(null);
 
+  // @ts-ignore
   return (
-    <VStack className="App">
-      <Center role="logo">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo"/>
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo"/>
-        </a>
-      </Center>
-      <Text fontSize='6xl'>Vite + React</Text>
-      <Counter/>
-      <Alert status='info' alignItems='center'
-             justifyContent='center'
-             textAlign='center'>
-        Click on the Vite and React logos to learn more
-      </Alert>
-    </VStack>
+    <React.Fragment>
+      <Flex gap={2}>
+        <IconButton ref={btnRef} aria-label="Menus" icon={<MdMenu/>} onClick={onOpen} size="sm"/>
+        <Button size="sm">Help</Button>
+
+        <Spacer/>
+        <IconButton aria-label="logout" title="logout" icon={<MdLogout/>} size="sm"/>
+
+      </Flex>
+      <Drawer
+        isOpen={isOpen}
+        placement='left'
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay/>
+        <DrawerContent>
+          <DrawerCloseButton/>
+          <DrawerHeader>Create your account</DrawerHeader>
+
+          <DrawerBody>
+            <Input placeholder='Type here...'/>
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant='outline' mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme='blue'>Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+      <VStack className="App">
+        <Center role="logo">
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo"/>
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo"/>
+          </a>
+        </Center>
+        <Text fontSize='6xl'>Vite + React</Text>
+        <Counter/>
+        <Alert status='info' alignItems='center'
+               justifyContent='center'
+               textAlign='center'>
+          Click on the Vite and React logos to learn more
+        </Alert>
+      </VStack>
+    </React.Fragment>
   )
 }
 
